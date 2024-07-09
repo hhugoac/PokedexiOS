aa1193ac9343274a8cd61c88301f8e95da5505d5
hectoralonzofg <hector_alonzo@fragua.com.mx>
2024-07-09 13:51:32 -0600

    Initial Commit

---
+++ PokedexApi/Controller/TabBarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokedexApi
+//
+//  Created by Hector Alonzo  on 09/07/24.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    //Todo:
+    // 1. Create the next items to the tabbar : Pokemon, Captures, Fav, Settings
+    // 2. Setup the initial list with cards in white
+    // 3. Setup the api-rest
+    
+
+
+}
+
